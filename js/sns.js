de3ae734940917626482dd5d789a1a455e7c6709
kakurega-labo <udon05.0025@gmail.com>
2026-03-31 19:16:31 +0900

    Update sns.js

--- js/sns.js
+++ js/sns.js
@@ -266,7 +266,7 @@ document.addEventListener('DOMContentLoaded', () => {
                           <svg class="w-8 h-8" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
                       </div>
                   </div>
-                  <div class="flex-grow max-w-2xl border-x border-slate-100">
+                  <div class="flex-grow max-w-2xl lg:mr-8 border-x border-slate-100">
                         ${getHeader(data)} <div class="relative">
                             <div class="h-32 md:h-48 bg-slate-300"></div>
                             <div class="px-4 pb-4">
