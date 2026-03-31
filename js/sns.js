7e684139f22a9202a5d2ebd0e70c067948a0fa1f
kakurega-labo <udon05.0025@gmail.com>
2026-03-31 19:27:35 +0900

    Update sns.js

--- js/sns.js
+++ js/sns.js
@@ -231,7 +231,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div class="flex-grow">
                         <div class="flex items-center gap-1">
                             <span class="font-bold text-sm select-none [--webkit-user-select:none] [--webkit-tap-highlight-color:transparent]">${data.name}</span>
-                                        <svg class="w-5 h-5 text-blue-400" fill="currentColor" viewBox="0 0 20 20"><path d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.64.304 1.25.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"/></svg>
                             <span class="text-slate-500 text-xs select-none [--webkit-user-select:none] [--webkit-tap-highlight-color:transparent]">${data.id} · ${post.date}</span>
                         </div>
                         <p class="text-sm mt-1 text-slate-800 leading-relaxed">${post.content}</p>
@@ -279,7 +278,6 @@ document.addEventListener('DOMContentLoaded', () => {
                                 <div>
                                     <h5 class="font-bold text-xl md:text-2xl flex items-center gap-1 select-none [--webkit-user-select:none] [--webkit-tap-highlight-color:transparent]">
                                         ${data.name} 
-                                        <svg class="w-5 h-5 text-blue-400" fill="currentColor" viewBox="0 0 20 20"><path d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.64.304 1.25.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"/></svg>
                                     </h5>
                                     <p class="text-slate-500 text-sm select-none [--webkit-user-select:none] [--webkit-tap-highlight-color:transparent]">${data.id}</p>
                                     <p class="text-sm mt-3 text-slate-800 leading-relaxed select-none [--webkit-user-select:none] [--webkit-tap-highlight-color:transparent]">${data.bio}</p>
